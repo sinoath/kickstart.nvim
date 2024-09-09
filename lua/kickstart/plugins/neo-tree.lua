c691cf267b51a4d2481416ef1a886dbf06680f02
Abraxas <p.buciuni@protonmail.com>
2024-09-09 06:07:46 +0200

    Enabled neotree as the default when nvim open a directory

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -10,6 +10,25 @@ return {
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
+  init = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      -- make a group to be able to delete it later
+      group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        if vim.fn.isdirectory(f) ~= 0 then
+          vim.cmd('Neotree current dir=' .. f)
+          -- now init autocmd can be deleted
+          vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+        end
+      end,
+    })
+  end,
+  opt = {
+    filesystem = {
+      hijack_netrw_behavior = 'open_current',
+    },
+  },
   -- keys = {
   --   { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   -- },
