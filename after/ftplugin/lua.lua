ded48ccf02c0e92d1cb6911c4727e2dd894ef217
Abraxas <p.buciuni@protonmail.com>
2024-09-17 19:44:39 +0200

    Added Lua execute shortcuts, now the terminal enter insert mode

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
 
-map('n', '<leader>xv', '<cmd>vsplit<CR><cmd>term lua %<CR>', { silent = true, desc = 'Split [v]ertically and Execute' })
-map('n', '<leader>xh', '<cmd>split<CR><cmd>term lua %<CR>', { silent = true, desc = 'Split [h]orizontally and Execute' })
-map('n', '<leader>xx', '<cmd>term lua %<CR>', { silent = true, desc = 'E[x]ecute the file full screen' })
+map('n', '<leader>xv', '<cmd>vsplit<CR><cmd>term lua %<CR>i', { silent = true, desc = 'Split [v]ertically and Execute' })
+map('n', '<leader>xh', '<cmd>split<CR><cmd>term lua %<CR>i', { silent = true, desc = 'Split [h]orizontally and Execute' })
+map('n', '<leader>xx', '<cmd>term lua %<CR>i', { silent = true, desc = 'E[x]ecute the file full screen' })
