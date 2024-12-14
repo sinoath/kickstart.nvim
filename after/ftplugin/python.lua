1d8f0d8cd83d0da36bf0bade98eb1a0a7880804b
Abraxas <p.buciuni@protonmail.com>
2024-12-14 19:43:26 +0100

    ftplugin/python.lua added xs shortcut for Save file and Execute

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -3,3 +3,4 @@ local map = vim.keymap.set
 map('n', '<leader>xv', '<cmd>vsplit<CR><cmd>term python3 %<CR>i', { silent = true, desc = 'Split [v]ertically and Execute' })
 map('n', '<leader>xh', '<cmd>split<CR><cmd>term python3 %<CR>i', { silent = true, desc = 'Split [h]orizontally and Execute' })
 map('n', '<leader>xx', '<cmd>term python3 %<CR>i', { silent = true, desc = 'E[x]ecute the file full screen' })
+map('n', '<leader>xs', ':w<CR><cmd>term python3 %<CR>i', { silent = true, desc = '[s]ave the file and e[x]ecute full screen' })
