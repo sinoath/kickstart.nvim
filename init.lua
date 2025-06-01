40f9855d69cc7acd8255441a77df2d7c0ace3639
Abraxas <p.buciuni@protonmail.com>
2025-06-01 09:48:17 +0200

    Updated neodev.nvim with the new lazydev.nvim

--- init.lua
+++ init.lua
@@ -337,7 +337,7 @@ require('lazy').setup({
 
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
+      { 'folke/lazydev.nvim', opts = {} },
     },
     config = function()
       -- Brief aside: **What is LSP?**
