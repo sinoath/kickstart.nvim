9206f86eaf69a8fb56295aa5316d72d8d5145274
Abraxas <p.buciuni@protonmail.com>
2025-06-01 09:45:39 +0200

    Added 'ensure_installed' and 'automatic_enabled' to mason-lspconfig
    both are required parameters inside the setup of mason-lspconfig

--- init.lua
+++ init.lua
@@ -568,6 +568,8 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = { 'lua_ls' },
+        automatic_enable = true,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
