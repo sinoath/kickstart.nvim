fad1c4ce94462e2cb9032379f2dcd94fb712bf95
WinAbraxas <p.buciuni@protonmail.com>
2025-06-26 06:09:56 +0200

    Moved the option to the top (consistency with WLS neovim config)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,8 +31,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Map <ESC> to 'jk' in insert mode:
-vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Set "jk" as ESC in insert mode'})
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
