6df3b9b50aa970ef825bd1d4e3bdeae017f28c4b
WinAbraxas <p.buciuni@protonmail.com>
2025-07-13 18:41:32 +0200

    Changed Lua LSP for Neovim APIs, lazydev in place of neodev

--- init.lua
+++ init.lua
@@ -330,9 +330,9 @@ require('lazy').setup({
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+      -- `lazydev` (in place of neodev) configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
+      { 'folke/lazydev.nvim', opts = {} },
     },
     config = function()
       -- Brief aside: **What is LSP?**
