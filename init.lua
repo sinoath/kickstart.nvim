7f36baa45e1b0767d5c340ea533a6d816e46742d
Abraxas <p.buciuni@protonmail.com>
2025-07-14 00:31:43 +0200

    Added global disable Lua diagnostic for 'missing-fields'

--- init.lua
+++ init.lua
@@ -124,6 +124,9 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
+-- Globally disable Lua diagnostic for 'missing-fields'
+---@diagnostic disable: missing-fields
+--
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
