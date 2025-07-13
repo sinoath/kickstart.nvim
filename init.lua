dc47a85fee3cbee4557ae26b96abf27f456163fb
WinAbraxas <p.buciuni@protonmail.com>
2025-07-13 18:49:14 +0200

    Added plugin render-markdown module, require it in init.lua

--- init.lua
+++ init.lua
@@ -817,6 +817,7 @@ require('lazy').setup({
   require 'custom.plugins.nvim-dap',
   require 'custom.plugins.nvim-dap-python',
   require 'custom.plugins.nvim-dap-ui',
+  require 'custom.plugins.render-markdown',
   require 'custom.colorschemes',
   -- { import = 'custom.colorschemes' },
 }, {
