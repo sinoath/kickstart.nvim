044cea3b99c19888e1d89c7dfd7c10584c4f84bd
Abraxas <p.buciuni@protonmail.com>
2025-07-23 06:57:38 +0200

    Disabled Lua diagnostic: deprecated

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,8 +18,10 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+---@diagnostic disable: deprecated
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+---@diagnostic enable: deprecated
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
