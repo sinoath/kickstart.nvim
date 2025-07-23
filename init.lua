82a72c827a46488a0cc1c9797c8f0eaedf572e74
Abraxas <p.buciuni@protonmail.com>
2025-07-23 05:13:02 +0200

    Disabled Lua diagnostic warning: undefined-field

--- init.lua
+++ init.lua
@@ -106,6 +106,7 @@ require 'keymaps'
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
